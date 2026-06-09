39004779dc6679d1f25852d97f4b33d3443ebefc
rpadofacci <rpadofacci@gmail.com>
2026-06-09 13:28:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,8 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'application/json');
-
-  if (req.url === '/' && req.method === 'GET') {
-    res.writeHead(200);
-    res.end(JSON.stringify({ status: 'ok' }));
-  } else {
-    res.writeHead(404);
-    res.end(JSON.stringify({ error: 'Not found' }));
-  }
+  res.writeHead(200);
+  res.end(JSON.stringify({ status: 'ok' }));
 });
 
 server.listen(PORT, () => {
