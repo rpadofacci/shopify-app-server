5126a57aa0c0c5d26043de2f0507cd7087ae3189
Renata <renata@Renatas-MacBook-Air.local>
2026-06-09 14:05:56 -0400

    update server

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'application/json');
+
   res.writeHead(200);
   res.end(JSON.stringify({ status: 'ok' }));
 });
